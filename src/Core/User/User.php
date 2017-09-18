0327ed2d34a02afff5d7d2b569bceae079de0116
railken <railken@protonmail.ch>
2017-09-18 18:33:50 +0200

    added passport

--- src/Core/User/User.php
+++ src/Core/User/User.php
@@ -2,15 +2,15 @@
 
 namespace Core\User;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Passport\HasApiTokens;
 use Railken\Laravel\Manager\ModelContract;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable implements ModelContract
 {
-    use Notifiable, SoftDeletes;
+    use HasApiTokens, Notifiable, SoftDeletes;
 
 	/**
 	 * The table associated with the model.
